5ea0ec50c12716797a7b68515b7d6b5c7d1d6b51
bob <vasyaovosh@gmail.com>
2022-09-06 02:59:52 +0800

    Added delete feature

--- src/ToDoTask.js
+++ src/ToDoTask.js
@@ -11,6 +11,7 @@ class ToDoTask extends React.Component {
 		}
 		
 		this.onTaskClick = this.onTaskClick.bind(this)
+		this.onTaskDeleteClick = this.onTaskDeleteClick.bind(this)
 	}
 	
 	onTaskClick(e){
@@ -19,7 +20,7 @@ class ToDoTask extends React.Component {
 		fetch(`http://127.0.0.1:3000/tasks/${this.props._id}`,{
 			method:'PATCH'
 		}).then(res=>{
-		if(res.status ==200) {
+		if(res.status ===200) {
 			this.setState({
 				done: !this.state.done
 			})
@@ -27,8 +28,20 @@ class ToDoTask extends React.Component {
 	})
 	}
 	
+	onTaskDeleteClick(e){
+		e.preventDefault()
+		
+		fetch(`http://127.0.0.1:3000/tasks/${this.props._id}`,{
+			method:'DELETE'
+		}).then(res=>{
+		if(res.status ==200) {
+			this.props.onTaskDelete(this.props._id)
+			}
+		})
+	}
+	
 	render(){
-		let task = <li onClick={this.onTaskClick}>{this.props.name}:{this.state.done ? 'Done' : 'Todo'}</li>
+		let task = <li><span onClick={this.onTaskDeleteClick}>X</span> <span onClick={this.onTaskClick}>{this.props.name}:{this.state.done ? 'Done' : 'Todo'}</span></li>
 		return task
 		}
 }
